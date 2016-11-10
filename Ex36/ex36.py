b2cd9413a2fd5a8c786203d00fa073bc1140dcc3
Henri Nehlsen <henrinehlsen@Henris-MacBook-Pro.fritz.box>
2016-11-10 02:43:40 +0100

    minor changes on ex36.py

--- Ex36/ex36.py
+++ Ex36/ex36.py
@@ -44,7 +44,7 @@ def phonecall():
     print "things for my wife, can you do that?\""
 
     while True:
-        answer_to_boss = raw_input(">>> ").lower()
+        phone_decision = raw_input(">>> ").lower()
 
         if phone_decision in positive:
             print "\"Great, I\'ll send you the details via email!\""
@@ -67,21 +67,23 @@ new_watch = "New Watch"
 prices = [1600, 875, 1450, 600]
 
 def open_email():
-    print "Your boss sent you an email:\n"
+    time.sleep(3)
+    print "There, your boss sent you an email:\n"
     print "\tDear %r" % define_player.player_name
     open_email.email = """\tPlease get my wife some of the following items.
-    She doesn\'t need all of them, so select only three out of four:\n
-    \t%r - %d€
-    \t%r - %d€
-    \t%r - %d€
-    \t%r - %d€\n
-    You can take my credit card, but don't spend too much money!
-    Best,
-    Your Boss!\n\n""" % (new_purse, prices[0], high_heels, prices[1], fancy_dress, prices[2], new_watch, prices[3])
+    \tShe doesn\'t need all of them, so select only three out of four:\n
+    \t\t%r - %d Euros
+    \t\t%r - %d Euros
+    \t\t%r - %d Euros
+    \t\t%r - %d Euros\n
+    \tYou can take my credit card, but don't spend too much money!
+    \tBest,
+    \tYour Boss!\n\n""" % (new_purse, prices[0], high_heels, prices[1], fancy_dress, prices[2], new_watch, prices[3])
     print open_email.email
+    time.sleep(2)
     print "You can buy everything online or go to the mall,"
-    print "it\'s your choice, %r" % define_player.player_name
-    print "what do you want to do?"
+    print "it\'s your choice, %r." % define_player.player_name
+    print "What do you want to do?"
 
     while True:
         shopping_decision = raw_input(">>> ").lower()
@@ -95,7 +97,7 @@ def open_email():
             print not_understood
 
 def online_shopping():
-    print "Cool, some online shopping! You\'re on Amazon now"
+    print "Cool, some online shopping! You\'re on Amazon now."
     time.sleep(1)
     eliminate_item()
 
@@ -103,41 +105,50 @@ def mall_shopping():
     print "Cool, we\'re getting out of our lazy chair."
     print "You take the train to the mall and you immediately regret it."
     time.sleep(1)
-    print "You arrive at the mall and you hate it, the minute you enter it."
+    print "\nYou arrive at the mall and you hate it, the minute you enter."
     print "Fortunately, you find a clerk that\'s willing to help you."
     time.sleep(1)
-    eliminate_item
+    eliminate_item()
 
-keyw_purse = ["purse"]
-keyw_heels = ["high", "heels", "shoes"]
-keyw_dress = ["fancy", "dress"]
-keyw_watch = ["watch"]
+keyw_purse = ["purse", "new purse"]
+keyw_heels = ["high", "high heels", "heels", "shoes"]
+keyw_dress = ["fancy", "fancy dress", "dress"]
+keyw_watch = ["watch", "new watch"]
 option_purse = (sum(prices) - prices[0])
 option_heels = (sum(prices) - prices[1])
 option_dress = (sum(prices) - prices[2])
 option_watch = (sum(prices) - prices[3])
 
 def success():
+    time.sleep(2)
     print "Well done, your boss and his wife are happy!!!\n"
+    credits_and_ending()
 
 def unsuccessful():
+    time.sleep(2)
     print "You spent too much money, your boss is angry!!!"
     print "You will never get a promotion...\n"
+    credits_and_ending()
 
 def credits_and_ending():
+    time.sleep(2)
     print "NOW GO BACK TO WORK!!!\n\n\n"
+    time.sleep(2)
     print "GAME OVER, THANKS FOR PLAYING!\n"
+    time.sleep(3)
     print "...all code written by Henri Nehlsen"
+    exit(0)
 
 def eliminate_item():
-    "First, you need to eliminate one of the items on the list."
-    "You forgot everything, right? Should we look at the email once again?"
+    print "First, you need to eliminate one of the items on the list."
+    print "\nYou can\'t remember anything, right? Should we look at the email once again?"
 
     while True:
         email_reminder = raw_input(">>> ").lower()
 
         if email_reminder in positive:
             print open_email.email
+            print "Ok then, which item do you want to eliminate?"
             break
         elif email_reminder in negative:
             print "Ok then, which item do you want to eliminate?"
@@ -150,24 +161,29 @@ def eliminate_item():
 
         if dismissed_item in keyw_purse:
             print "Well, then, you bought:"
-            print """Beautiful %r, a pretty
-            neat %r and a super cool %r""" % (high_heels, fancy_dress, new_watch)
-            print "You spent %d€" % option_purse
+            print """Beautiful %r, a pretty neat %r and a super cool %r""" % (high_heels, fancy_dress, new_watch)
+            time.sleep(2)
+            print "\nYou spent %d Euros" % option_purse
+            success()
         elif dismissed_item in keyw_heels:
             print "Well, then, you bought:"
-            print """A beautiful %r, a pretty
-            neat %r and a super cool %r""" % (new_purse, fancy_dress, new_watch)
-            print "You spent %d€" % option_heels
+            print """A beautiful %r, a pretty neat %r and a super cool %r""" % (new_purse, fancy_dress, new_watch)
+            time.sleep(2)
+            print "\nYou spent %d Euros" % option_heels
+            unsuccessful()
         elif dismissed_item in keyw_dress:
             print "Well, then, you bought:"
-            print """A beautiful %r, some pretty
-            neat %r and a super cool %r""" % (new_purse, high_heels, new_watch)
-            print "You spent %d€" % option_dress
+            print """A beautiful %r, some pretty neat %r and a super cool %r""" % (new_purse, high_heels, new_watch)
+            time.sleep(2)
+            print "\nYou spent %d Euros" % option_dress
+            success()
         elif dismissed_item in keyw_watch:
             print "Well, then, you bought:"
-            print """Beautiful %r, a pretty
-            neat %r and a brand new %r""" % (new_purse, high_heels, fancy_dress)
-            print "You spent %d€" % option_watch
+            print """Beautiful %r, a pretty neat %r and a brand new %r""" % (new_purse, high_heels, fancy_dress)
+            time.sleep(2)
+            print "\nYou spent %d Euros" % option_watch
+            time.sleep
+            unsuccessful()
         else:
             print not_understood
 
